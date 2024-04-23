fa11a52a798b441daaf46b0c720caa717bfb4e9c
Basselalshawareb <basselalshawareb3@gmail.com>
2024-04-24 00:12:16 +0300

    Model inferencer. Detect and draw

---
+++ GUI/inferencer.py
@@ -0,0 +1,21 @@
+import mmcv
+from mmengine.registry import DATASETS
+from mmengine.config import Config
+from mmdet.utils import register_all_modules
+from mmengine.runner import Runner
+from mmdet.apis.inference import inference_detector, init_detector
+from mmdet.visualization import DetLocalVisualizer
+class Inferencer():
+    def __init__(self, cfg, ckpt) -> None:
+        self.model = init_detector(cfg, ckpt)
+        self.visualizer = DetLocalVisualizer()
+    
+    def infer(self, img):
+        datasample = inference_detector(self.model, img)
+        self.visualizer.add_datasample(name='pred', image=img, data_sample=datasample, draw_gt=False)
+        img = self.visualizer._image 
+        return img
+
+
+if __name__=='__main__':
+    register_all_modules()
