5e2dfb47b5207b299adae2bd2d38b9eed517ca5c
Basselalshawareb <basselalshawareb3@gmail.com>
2024-07-02 13:55:04 +0300

    Setup defect package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+
+if __name__=="__main__":
+    setup(
+        name="Defect",
+        version="0.1",
+        packages=find_packages(include=['defect'])
+    )
