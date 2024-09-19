bbded2b65cfb497beb14885433b6be914145ea2e
Basselalshawareb <basselalshawareb3@gmail.com>
2024-09-19 12:03:12 +0300

    Inference from folder

--- infer.py
+++ infer.py
@@ -30,19 +30,19 @@ for img_path in imgs_paths:
     results = inference_detector(model, img)
     results.pred_instances = results.pred_instances.detach().cpu()
     bboxes = results.pred_instances.bboxes
-    # ids = []
-    # for i in range(bboxes.shape[0]):
-    #     x1,y1,x2,y2 = bboxes[i]
-    #     mask = torch.bitwise_and(torch.bitwise_and(x1 < bboxes[:,0], y1 < bboxes[:,1]), torch.bitwise_and(x2 > bboxes[:,2], y2 > bboxes[:,3]))
-    #     if mask.any():
-    #         continue
-    #     ids.append(i)
-    # if len(ids) != 0:
-    #     pred_instances = InstanceData(metainfo=dict(img_shape=(800, 1196, 3)))
-    #     pred_instances.bboxes = results.pred_instances.bboxes[ids]
-    #     pred_instances.labels = results.pred_instances.labels[ids]
-    #     pred_instances.scores = results.pred_instances.scores[ids]
-    #     results.pred_instances = pred_instances
+    ids = []
+    for i in range(bboxes.shape[0]):
+        x1,y1,x2,y2 = bboxes[i]
+        mask = torch.bitwise_and(torch.bitwise_and(x1 < bboxes[:,0], y1 < bboxes[:,1]), torch.bitwise_and(x2 > bboxes[:,2], y2 > bboxes[:,3]))
+        if mask.any():
+            continue
+        ids.append(i)
+    if len(ids) != 0:
+        pred_instances = InstanceData(metainfo=dict(img_shape=(800, 1196, 3)))
+        pred_instances.bboxes = results.pred_instances.bboxes[ids]
+        pred_instances.labels = results.pred_instances.labels[ids]
+        pred_instances.scores = results.pred_instances.scores[ids]
+        results.pred_instances = pred_instances
     
     output = vis._draw_instances(img, results.pred_instances[results.pred_instances.scores>0.7], classes=CLASSES, palette=palette)
     cv2.imwrite(f"cam_out_scratches/{img_path}",output)
